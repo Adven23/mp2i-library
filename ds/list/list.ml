38d80d353228d05d2edac34e243b645fc76f0b03
Alexandre Gauthe <89643198+Adven23@users.noreply.github.com>
2021-11-12 18:38:22 +0100

    Delete list.ml

--- ds/list/list.ml
+++
@@ -1,9 +0,0 @@
-(* [somme l] renvoie la somme des éléments de [l] *) 
-let rec somme l = match l with 
-    | [] -> 0 
-    | e::q -> e + somme q;; 
-    
-(* [appartient e l] renvoie true si l'élément [e] appartient à la liste [l], false sinon *) 
-let rec appartient e l = match l with 
-    |[] -> false 
-    |x::q -> x=q || appartient e q ;;
