d1f51365715adaa00021635f7b9095024aa1b21e
Alexandre Gauthe <89643198+Adven23@users.noreply.github.com>
2021-11-13 21:42:39 +0100

    Create tableau_dynamique.ml

---
+++ structures de données/tableau_dynamique/tableau_dynamique.ml
@@ -0,0 +1,18 @@
+(* création du tableau dynamique *)
+type 'a t_dyn = {mutable t: 'a array; mutable n: int} ;;
+
+(* fonction auxiliaire permettant de copier le tableau [t1] dans le tableau [t2] *)
+let copie t1 t2 = 
+    for i=0 to Array.length t1 - 1 do 
+        t2.(i) <- t1.(i) 
+    done;;
+
+(* [add e d] permet d'ajouter un élément [e] dans le tableau dynamique [d] *)
+let add e d = 
+    d.n <- d.n + 1 ;
+    if d.n < Array.length d.t then d.t.(d.n) <- e
+    else if d.n = 0 then d.t <- [|e|] 
+    else let t' = Array.make (2*d.n) 0 in 
+    (copie d.t t';
+    d.t <- t';
+    d.t.(d.n) <- e)
