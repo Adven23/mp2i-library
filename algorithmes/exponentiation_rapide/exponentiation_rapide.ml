eee0a13b3601ad8c1727452b78aef5e06f00c4b5
Alexandre Gauthe <89643198+Adven23@users.noreply.github.com>
2021-11-16 22:34:09 +0100

    Create exponentiation_rapide.ml

---
+++ algorithmes/exponentiation_rapide/exponentiation_rapide.ml
@@ -0,0 +1,6 @@
+(* [exp_rapide a n] calcule a^n en petite complexité *) 
+let rec exp_rapide a n = 
+    if n = 0 then 1 
+    else let b = exp_rapide a (n/2) in 
+    if n mod 2 = 0 then b*b
+    else a*b*b
